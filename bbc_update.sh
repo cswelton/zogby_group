863a98956530d7023734d8ee5fa642c37a9aa209
Craig Welton <crwelton@cisco.com>
2021-04-22 14:00:06 -0400

    updating bbc update script

--- bbc_update.sh
+++ bbc_update.sh
@@ -5,16 +5,16 @@
 # It is meant to be run in a cron job to automatically update the website following play.
 
 echo "Checking for new round data...";
-bbc-sync --results-directory='./results' ekjusj;
+/Users/crwelton/virtualenv/golfgenius/bin/bbc-sync --results-directory='./results' ekjusj;
 retVal=$?;
 
 if [ $retVal -eq 0 ]; then
     echo "round data synced, regenerating website data...";
-    bbc-stats --results-directory='./results' --github-site='./website_root' --points-config-file='./website_root/_data/points_config.json'
+    /Users/crwelton/virtualenv/golfgenius/bin/bbc-stats --results-directory='./results' --github-site='./website_root' --points-config-file='./website_root/_data/points_config.json'
     retVal=$?;
     if [ $retVal -eq 0 ]; then
         echo "Building website...";
-        jekyll build --source='./website_root' --destination='./docs'
+        /Users/crwelton/virtualenv/golfgenius/bin/jekyll build --source='./website_root' --destination='./docs'
         retVal=$?;
         if [ $retVal -eq 0 ]; then
             echo "Deploying website...";
