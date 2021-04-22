308bc3f44aa124ad26130df0c89613a0c13ffd69
Craig Welton <crwelton@cisco.com>
2021-04-22 13:38:20 -0400

    adds bbc_update script

---
+++ bbc_update.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# This will run the sync and if new round data is synced, rebuild and deploy the website.
+# This script needs to be run from the root of the zogby_group repository.
+# It is meant to be run in a cron job to automatically update the website following play.
+
+echo "Checking for new round data...";
+bbc-sync --results-directory='./results' nzxmej;
+retVal=$?;
+
+if [ $retVal -eq 0 ]; then
+    echo "round data synced, regenerating website data...";
+    bbc-stats --results-directory='./results' --github-site='./website_root' --points-config-file='./website_root/_data/points_config.json'
+    retVal=$?;
+    if [ $retVal -eq 0 ]; then
+        echo "Building website...";
+        jekyll build --source='./website_root' --destination='./docs'
+        retVal=$?;
+        if [ $retVal -eq 0 ]; then
+            echo "Deploying website...";
+            git add --all && git commit -m 'Updating Website using bbc_update.sh' && git push origin master;
+        fi
+    fi
+else
+    echo "All Caught Up";
+    exit $retVal;
+fi
+
