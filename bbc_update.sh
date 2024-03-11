7eed4bfae096cd0067cb8f273c458abb53384ad6
Craig Welton <crwelton@cisco.com>
2024-03-11 15:21:35 -0400

    updates bin locations

--- bbc_update.sh
+++ bbc_update.sh
@@ -5,16 +5,16 @@
 # It is meant to be run in a cron job to automatically update the website following play.
 
 echo "Checking for new round data...";
-/Users/crwelton/virtualenv/golfgenius3.10.10/bin/bbc-sync --results-directory='./results' uspfuw;
+/Users/crwelton/virtualenv/golfgenius2/bin/bbc-sync --results-directory='./results' uspfuw;
 retVal=$?;
 
 if [ $retVal -eq 0 ]; then
     echo "round data synced, regenerating website data...";
-    /Users/crwelton/virtualenv/golfgenius3.10.10/bin/bbc-stats --results-directory='./results' --github-site='./website_root' --points-config-file='./website_root/_data/points_config.json' --blacklist-rounds-file='./website_root/_data/BLACKLISTED_ROUNDS'
+    /Users/crwelton/virtualenv/golfgenius2/bin/bbc-stats --results-directory='./results' --github-site='./website_root' --points-config-file='./website_root/_data/points_config.json' --blacklist-rounds-file='./website_root/_data/BLACKLISTED_ROUNDS'
     retVal=$?;
     if [ $retVal -eq 0 ]; then
         echo "Building website...";
-        /Users/crwelton/.rubies/ruby-3.1.3/bin/bundle exec /Users/crwelton/.rubies/ruby-3.1.3/bin/jekyll build --source='./website_root' --destination='./docs'
+        /Users/crwelton/.gem/ruby/3.1.3/bin/bundle exec /Users/crwelton/.gem/ruby/3.1.3/bin/jekyll build --source='./website_root' --destination='./docs'
         retVal=$?;
         if [ $retVal -eq 0 ]; then
             echo "Deploying website...";
